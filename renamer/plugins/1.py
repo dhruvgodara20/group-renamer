71263be26bae19dfdd3e5ecead1aae2559964824
dhruvgodara20 <78217376+dhruvgodara20@users.noreply.github.com>
2021-06-09 16:29:31 +0530

    Update 1.py

--- renamer/plugins/1.py
+++ renamer/plugins/1.py
@@ -19,7 +19,7 @@ async def force_sub(c, m):
         except UserNotParticipant:
             button = [[InlineKeyboardButton('join Updates channel', url=f'https://t.me/{Config.FORCE_SUB}')]]
             markup = InlineKeyboardMarkup(button)
-            return await m.reply_text(text="Hey join in my updates channel to use me.", parse_mode='markdown', reply_markup=markup, quote=True)
+            return await m.reply_text(text="Hey Join My Updates Channel to use me.", parse_mode='markdown', reply_markup=markup, quote=True)
 
         except ChatAdminRequired:
             logger.warning(f"Make me admin in @{Config.FORCE_SUB}")
@@ -37,7 +37,7 @@ async def force_sub(c, m):
                 if m.from_user.id in Config.AUTH_USERS:
                     return await m.reply_text("Forcesub username must be a channel username Not yours or any other users username")
             logger.error(e)
-            return await m.reply_text("Some thing went wrong. Try again and if same issue occur contact [our group](https://t.me/Ns_Bot_supporters)", disable_web_page_preview=True, quote=True)
+            return await m.reply_text("Some thing went wrong. Try again and if same issue occur contact [our group](https://t.me/Its_My_leech)", disable_web_page_preview=True, quote=True)
 
     await m.continue_propagation()
 
